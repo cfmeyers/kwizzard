c50544077f1f6cb9a1c63df8ebc2bbff8bcb89fc
cfmeyers <cfmeyers@gmail.com>
2015-04-13 01:36:53 -0400

    fix up cli rendering

--- kwiz_cli.rb
+++ kwiz_cli.rb
@@ -2,14 +2,22 @@ require_relative './lib/kwizard'
 require 'pry'
 
 kwizard = Kwizard.new('./questions')
+clear_code = %x{clear}
+topic = ARGV.shift
 
-# topic = ARGV[0]
+abort('The topic you passed as an argument has no questions') unless kwizard.topics.include? topic
+print clear_code
+
+puts "Welcome to Kwizard.  Prepare to be kwizzed..."
 
 while true
-  # puts topic
-  dialogue = kwizard.random_dialogue('python')
+  dialogue = kwizard.random_dialogue(topic)
   puts dialogue.question
+  puts '================ hit any key to see the answer ================'
   gets.chomp
   puts dialogue.answer
-  gets.chomp
+  puts 'hit "q" to quit, any other key to go to next question'
+  response = gets.chomp
+  abort('Thank you for playing') if response == 'q' 
+  print clear_code
 end
